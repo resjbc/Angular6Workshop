f5b22b5d4dc8825bf7c96cecd550490acf5930db
Hitman <31611759+resjbc@users.noreply.github.com>
2018-08-20 21:38:37 +0700

    31%

--- src/app/authentication/components/profile/profile.component.ts
+++ src/app/authentication/components/profile/profile.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { IProfileComponent } from './profile.interface';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements IProfileComponent {
+ 
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  positionItem: any[] = [
+    'Frontend Developer',
+    'Backend Developer'
+  ];
 
 }
