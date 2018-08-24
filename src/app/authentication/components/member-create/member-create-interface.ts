b5b126bc81ede276e6c7b577b815a840149b22ba
ResHitman <resjbc@hotmail.com>
2018-08-24 10:37:19 +0700

    4-10

--- src/app/authentication/components/member-create/member-create-interface.ts
+++ src/app/authentication/components/member-create/member-create-interface.ts
@@ -1,8 +1,12 @@
 import { IRoleAccount } from "../../../shareds/services/account.service";
+import { FormGroup } from "@angular/forms";
 
 export interface IMemberCoponent {
     positionItem: string[];
     roleItem: IRoleAccount[];
 
+    form: FormGroup;
+
     getRoleName(role: IRoleAccount): string;
+    onSubmit(): void; 
 }
--- src/app/authentication/components/member-create/member-create.component.ts
+++ src/app/authentication/components/member-create/member-create.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IMemberCoponent } from './member-create-interface';
 import { IRoleAccount } from '../../../shareds/services/account.service';
 import { SharedsService } from '../../../shareds/services/shareds.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-member-create',
@@ -10,21 +11,46 @@ import { SharedsService } from '../../../shareds/services/shareds.service';
 })
 export class MemberCreateComponent implements IMemberCoponent {
 
-  constructor(private shareds: SharedsService) {
+  constructor(
+    private shareds: SharedsService,
+    private builder: FormBuilder
+  ) {
     this.positionItem = shareds.positionItem;
-   }
+    this.initailFormData();
+  }
+
+  form: FormGroup;
 
   positionItem: any[] = [];
   roleItem: IRoleAccount[] = [
     IRoleAccount.Member,
     IRoleAccount.Employee,
     IRoleAccount.Admin
- 
+
   ];
 
   //แสดงสิทธิ์ข้อมูลผู้ใช้เป็นตัวหนังสือ
   getRoleName(role: IRoleAccount): string {
     return IRoleAccount[role];
+  }
+
+  //สร้าง form
+  private initailFormData() {
+    this.form = this.builder.group({
+      email: [],
+      password: [],
+      firstname: [],
+      lastname: [],
+      position: [''],
+      role: [''],
+      image: []
+
+    });
+  }
+
+  //บันทึกหรือแก้ไขข้อมูล
+  onSubmit() {
+    console.log(this.form.value);
   }
 
 
