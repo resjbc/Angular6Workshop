0e530588df9eb380a858df33a274fd5625c7c2cb
Hitman <31611759+resjbc@users.noreply.github.com>
2018-09-02 20:12:26 +0700

    5-3

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,19 @@
-import { Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { AppURL } from './app.url';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AuthenticationGuard } from './guards/authentication.guard';
+import { UnauthenticationGuard } from './guards/unauthentication.guard';
 
 
 const RouteLists: Routes = [
     { path: '', redirectTo: AppURL.Login, pathMatch: 'full' },
-    { path: AppURL.Login, component: LoginComponent },
-    { path: AppURL.Register, component: RegisterComponent },
-    { 
-        path: AppURL.Authen, 
+    { path: AppURL.Login, component: LoginComponent, canActivate: [UnauthenticationGuard] },
+    { path: AppURL.Register, component: RegisterComponent, canActivate: [UnauthenticationGuard] },
+    {
+        path: AppURL.Authen,
         loadChildren: './authentication/authentication.module#AuthenticationModule',
-        canActivate: [AuthenticationGuard] 
+        canActivate: [AuthenticationGuard]
     }
 ];
 
